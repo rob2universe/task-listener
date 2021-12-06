eab58e9c381f90667d636480f8850dd0878789ec
rob2universe <rob2universe@gmail.com>
2021-12-06 14:17:50 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# vanilla-camunda-template
-
-This is a template repository to create a plain vanilla CAMUNDA Platform EE Spring Boot project.
-
-This is the best generic starting point!  - Change my mind :)
+# CAMUNDA task-listern example
--- src/test/java/org/camunda/test/ProcessTest.java
+++ src/test/java/org/camunda/test/ProcessTest.java
@@ -2,12 +2,16 @@ package org.camunda.test;
 
 
 import com.camunda.example.service.LoggerDelegate;
+import com.camunda.example.service.LoggerTaskListener;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.engine.test.mock.Mocks;
 import org.camunda.bpm.spring.boot.starter.test.helper.AbstractProcessEngineRuleTest;
 import org.camunda.spin.plugin.impl.SpinProcessEnginePlugin;
+import org.camunda.spin.plugin.variable.SpinValues;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.List;
@@ -15,19 +19,26 @@ import java.util.List;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
 
 @Deployment(resources = "process.bpmn")
-public class ProcessTest extends AbstractProcessEngineRuleTest {
+public class ProcessTest  {
+
+  @Rule
+  public ProcessEngineRule processEngineRule = new ProcessEngineRule();
+
 
   @Before
   public void setUp() {
     // processEngine.getProcessEngineConfiguration().setProcessEnginePlugins(List.of(new SpinProcessEnginePlugin()));
-    Mocks.register("logger", new LoggerDelegate());
+    Mocks.register("listener", new LoggerTaskListener());
   }
 
   @Test
   public void testHappyPath() {
 
-    ProcessInstance pi = runtimeService().startProcessInstanceByKey("example-process", withVariables("myVar", "myVarValue"));
-    assertThat(pi).isEnded();
-  }
+    String JSON = "[\"a\",\"b\",\"c\"]";
 
+    ProcessInstance pi = runtimeService().startProcessInstanceByKey("example-process",
+        withVariables("myList",SpinValues.jsonValue(JSON).create()));
+
+    assertThat(pi).isWaitingAt("DoSomethingTask");
+  }
 }
